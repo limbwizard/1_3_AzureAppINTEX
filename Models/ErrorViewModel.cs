f7b605545b23963e24edf13bcb797485ed2df88a
Isaac Limb <isaacdalelimb@gmail.com>
2024-04-07 11:06:43 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureAppINTEX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
