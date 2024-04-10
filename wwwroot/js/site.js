125fe20228d6886ab337d30f02372ca7f88b3602
Isaac Limb <isaacdalelimb@gmail.com>
2024-04-09 19:35:10 -0600

    fixed cookies hopefully

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var consent = getCookie("userConsent");
+    if (consent == null) {
+        document.getElementById("cookieConsent").style.display = "block";
+    } else {
+        document.getElementById("cookieConsent").style.display = "none";
+    }
+
+    var acceptButton = document.getElementById("acceptCookieConsent");
+    if (acceptButton) {
+        acceptButton.addEventListener("click", function () {
+            setCookie("userConsent", "true", 365); // Set the consent cookie for 1 year
+            document.getElementById("cookieConsent").style.display = "none";
+        });
+    }
+});
