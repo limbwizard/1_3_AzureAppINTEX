a9f11f2d8fffa18b61decd0638cbbc53a5fefeb5
RumitOsu <timurkherlenbayar@gmail.com>
2024-04-09 18:32:24 -0600

    Each item is now clickable and you can access the item page for each item blah blah

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AzureAppINTEX.Models;
+// Include any other necessary namespaces
+
+namespace AzureAppINTEX.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IStoreRepository _repository;
+
+        public ProductController(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Item(int id)
+        {
+            var product = _repository.Products.FirstOrDefault(p => p.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
